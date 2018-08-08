0ea8ee8c4228dbaa875d33fcfb17f90cb70f445c
axelmadrazo <axelmadrazo@gmail.com>
2018-08-07 23:37:54 -0500

    c

--- index.php
+++ index.php
@@ -24,7 +24,14 @@ $app = new Slim\App();
  */
 $app->GET('/supplierBranches/{partnerSupplierBranchId}/sales/{partnerSaleId}', function($request, $response, $args) {
             $headers = $request->getHeaders();
-            
+            $requestIdentifier = $headers['HTTP_X_REQUEST_IDENTIFIER'][0];
+            $xrequestauthentication = $headers['HTTP_X_REQUEST_AUTHENTICATION'][0];
+            $Accept = $headers['HTTP_ACCEPT'][0];
+
+            $partnerSupplierBranchId = $request->getAttribute('partnerSupplierBranchId');
+			$partnerSaleId = $request->getAttribute('partnerSaleId');
+
+
             $sql = "SELECT * FROM Sales";
 
             try{
@@ -72,9 +79,6 @@ $app->POST('/supplierBranches/{partnerSupplierBranchId}/sales', function($reques
             $partnerSupplierBranchId = $request->getAttribute('partnerSupplierBranchId');
 
             $body = $request->getParsedBody();
-
-
-        
 
             $sql = "INSERT INTO Sales (referenceId, partnerActivityId, partnerOfferId, localDate, partnerTicketTypeId, travelerCount, voucherCount, firstName, lastName, emailAddress, phoneNumber, holdDurationSeconds) VALUES
             (:referenceId,:partnerActivityId,:partnerOfferId,:localDate,:partnerTicketTypeId,:travelerCount,:voucherCount, :firstName, :lastName, :emailAddress, :phoneNumber, :holdDurationSeconds)";
@@ -161,9 +165,13 @@ $app->POST('/supplierBranches/{partnerSupplierBranchId}/sales', function($reques
  */
 $app->PUT('/supplierBranches/{partnerSupplierBranchId}/sales/{partnerSaleId}/cancellation/{cancellationCode}', function($request, $response, $args) {
             $headers = $request->getHeaders();
+            $requestIdentifier = $headers['HTTP_X_REQUEST_IDENTIFIER'][0];
+            $xrequestauthentication = $headers['HTTP_X_REQUEST_AUTHENTICATION'][0];
+            $Accept = $headers['HTTP_ACCEPT'][0];
             
-            
-
+            $partnerSupplierBranchId = $request->getAttribute('partnerSupplierBranchId');
+			$partnerSaleId = $request->getAttribute('partnerSaleId');
+            $cancellationCode: = $request->getAttribute('cancellationCode:');
 
 
 
